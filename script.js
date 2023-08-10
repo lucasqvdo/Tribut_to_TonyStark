79cb7b11d2e1f88074703b6c53e5f3e40995d846
Lucas Quevedo <87128880+lucasqvdo@users.noreply.github.com>
2023-08-10 14:26:58 -0300

    Update script.js
    Editado para que o carrossel funcione corretamente.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const imgs = document.querySelector(".container");
+  const img = document.querySelectorAll(".container img");
 
-const imgs = document.querySelector(".container");
-const img = document.querySelectorAll(".container img");
+  let idx = 0;
 
-let idx = 0;
+  function carrossel() {
+    idx = (idx + 1) % img.length;
+    imgs.style.transform = `translateX(${-idx * 400}px)`;
+  }
 
-function carrossel() {
-  idx = (idx + 1) % img.length;
-  imgs.style.transform = `translateX(${-idx * 400}px)`;
-}
-
-setInterval(carrossel, 1800);
+  setInterval(carrossel, 1800);
+});
